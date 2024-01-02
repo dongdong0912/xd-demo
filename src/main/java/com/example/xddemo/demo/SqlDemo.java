c0299eac6ad908270f19e3cee44a0ddc08ced971
xuedong <1162400912@qq.com>
2024-01-02 17:40:52 +0800

    增加一些工具类

--- src/main/java/com/example/xddemo/demo/SqlDemo.java
+++ src/main/java/com/example/xddemo/demo/SqlDemo.java
@@ -1,9 +1,11 @@
 package com.example.xddemo.demo;
 
+import net.sf.jsqlparser.expression.Function;
 import net.sf.jsqlparser.parser.CCJSqlParserUtil;
 import net.sf.jsqlparser.statement.select.OrderByElement;
 import net.sf.jsqlparser.statement.select.PlainSelect;
 import net.sf.jsqlparser.statement.select.Select;
+import net.sf.jsqlparser.statement.select.SelectExpressionItem;
 
 import java.util.List;
 
@@ -21,8 +23,26 @@ public class SqlDemo {
         System.out.println(orderElements);
         // 移除排序部分
         selectBody.setOrderByElements(null);
+        selectBody.setSelectItems(null);
+
+
         //  移除分组部分
         /*plainSelect.setGroupByElement(null);*/
+
+
+
+        // 创建 COUNT(*) 函数表达式
+        Function countFunction = new Function();
+        countFunction.setName("COUNT");
+        countFunction.setAllColumns(true);
+
+        // 创建 SelectExpressionItem，并将 COUNT(*) 函数表达式设置为其表达式
+        SelectExpressionItem countItem = new SelectExpressionItem(countFunction);
+
+
+        selectBody.addSelectItems(countItem);
+
+
         System.out.println(select);
     }
 }

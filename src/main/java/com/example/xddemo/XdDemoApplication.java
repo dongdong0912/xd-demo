b67ba69102b1cef0138a9866b2c89b785648e0e3
薛栋 <xuedong@xuedongdeMacBook-Pro.local>
2024-01-02 15:59:22 +0800

    初始化

---
+++ src/main/java/com/example/xddemo/XdDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.xddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XdDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XdDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/xddemo/XdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XdDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

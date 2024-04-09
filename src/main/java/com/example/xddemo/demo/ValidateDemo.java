456de727f32c944468f820b63c0220433687e1b4
xuedong <1162400912@qq.com>
2024-04-09 16:18:16 +0800

    硅基最终的版本

--- src/main/java/com/example/xddemo/demo/ValidateDemo.java
+++ src/main/java/com/example/xddemo/demo/ValidateDemo.java
@@ -3,7 +3,6 @@ package com.example.xddemo.demo;
 import lombok.Data;
 
 import javax.validation.constraints.Min;
-import java.util.List;
 
 /**
  * Author: xuedong
@@ -20,15 +19,54 @@ public class ValidateDemo {
         Person  person=new Person();
 
 
-        List<String> strings = ValidatorUtils.validateEntity(person);
+        Person1  person1=new Person1();
 
-        System.out.println(strings.toString());
+
+
+
+
 
 
     }
 
 
 
+    /**
+     * 普通模式
+     */
+    public void test_vo2dto01(Person person) {
+
+        Person1  person1=new Person1();
+        person1.setName(person.getName());
+        person1.setAge(person.getAge());
+        person1.setVal(person.getVal());
+        person1.setA(person.getB());
+
+
+    }
+
+
+
+
+
+
+
+    @Data
+    static class Person1 {
+
+        private String name;
+
+
+        @Min(value = 1, message = "用药天数必须大于等于1")
+        private Integer age;
+
+
+        private Float val;
+
+
+
+        private String a;
+    }
 
 
     @Data
@@ -42,5 +80,8 @@ public class ValidateDemo {
 
 
         private Float val;
+
+
+        private String b;
     }
 }

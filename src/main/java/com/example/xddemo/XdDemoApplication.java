9c9be7f04327dafc03aa69bed69299a8d96ab959
xuedong <1162400912@qq.com>
2024-07-11 11:17:48 +0800

    接入websocket

--- src/main/java/com/example/xddemo/XdDemoApplication.java
+++ src/main/java/com/example/xddemo/XdDemoApplication.java
@@ -1,19 +1,18 @@
 package com.example.xddemo;
 
-import com.example.xddemo.demo.AppProperties;
 import org.apache.rocketmq.spring.autoconfigure.RocketMQAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
-
-import javax.annotation.Resource;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(scanBasePackages = "com.example.xddemo", exclude = {RedisAutoConfiguration.class, RocketMQAutoConfiguration.class})
+// 扫描cn.hutool.extra.spring包下所有类并注册之
+@ComponentScan(basePackages={"cn.hutool.extra.spring","com.example.xddemo"})
 public class XdDemoApplication {
 
 
-    @Resource
-    private AppProperties  appProperties;
+
 
 
     public static void main(String[] args) {
--- src/main/java/com/example/xddemo/demo/AppProperties.java
+++ src/main/java/com/example/xddemo/demo/AppProperties.java
@@ -1,67 +1,55 @@
 package com.example.xddemo.demo;
 
-import com.alibaba.fastjson.JSON;
 import lombok.Data;
+import org.springframework.beans.factory.BeanFactoryUtils;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.util.Map;
 
 @Component
 @ConfigurationProperties(prefix = "app")
-public class AppProperties {
+@Data
+public class AppProperties implements ApplicationContextAware {
+
+
+    private transient ApplicationContext applicationContext;
 
     private String name;
-    private Server server = new Server();
+    private Father father;
 
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
 
 
     @PostConstruct
-    public void init(){
-        String jsonString = JSON.toJSONString(this);
-        System.out.println(jsonString);
+    public void init() {
+
+        System.out.println(this.getFather().getName());
+
+        Map<String, Father> providerConfigMap = applicationContext == null ? null : BeanFactoryUtils.beansOfTypeIncludingAncestors(applicationContext, Father.class, false, false);
+
+        System.out.println(providerConfigMap);
+
+
+        //Father bean = SpringUtil.getBean(Father.class);
+        //System.out.println(bean.toString());
+
+        // Server server1 = SpringUtil.getBean("server");
+
+       // System.out.println(server1.toString());
+
     }
 
-    // Getters and setters
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
 
-    public Server getServer() {
-        return server;
-    }
 
-    public void setServer(Server server) {
-        this.server = server;
-    }
 
-    @Data
-    public static class Server {
-        private String address;
-        private int port;
 
-        private Boolean isDefault;
-
-        // Getters and setters
-        public String getAddress() {
-            return address;
-        }
-
-        public void setAddress(String address) {
-            this.address = address;
-        }
-
-        public int getPort() {
-            return port;
-        }
-
-        public void setPort(int port) {
-            this.port = port;
-        }
-    }
 }
 

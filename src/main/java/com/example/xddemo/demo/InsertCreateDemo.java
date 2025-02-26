268ebf0b2923d865738f1b1f38a680054c817868
xuedong <1162400912@qq.com>
2025-02-26 14:14:30 +0800

    refactor(xddemo): 重构插入创建演示代码
    - 重命名 Demo 类为 InsertCreateDemo，更准确地反映其功能
    - 添加 COMMA 常量，减少硬编码并提高可维护性

---
+++ src/main/java/com/example/xddemo/demo/InsertCreateDemo.java
@@ -0,0 +1,53 @@
+package com.example.xddemo.demo;
+
+import cn.hutool.core.io.FileUtil;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * Author: xuedong
+ * Date: 2024/7/16
+ */
+public class InsertCreateDemo {
+
+
+    public static final String COMMA = ",";
+
+
+    public static void main(String[] args) throws Throwable {
+
+
+        // 指定要读取的文件路径
+        String filePath = "/Users/xuedong/Desktop/export_result.txt";
+
+        // 使用FileUtil按行读取文件
+        List<String> lines = FileUtil.readLines(new File(filePath), "UTF-8");
+
+        for (String line : lines) {
+            String[] split = line.split(COMMA);
+            String id = generate(split[0]);
+            String name = generate(split[1]);
+            String hospital_id = generate(split[2]);
+            String hospital_name = generate(split[3]);
+            String is_online = split[4];
+
+            String r = "(" + id + COMMA + name + COMMA + hospital_id + COMMA + hospital_name + COMMA + is_online + COMMA + "0" + "," + "now()" + "," + "now()" + ")" + COMMA;
+            System.out.println(r);
+        }
+
+
+    }
+
+
+    private static String generate(String content) {
+        return String.format("'%s'", content.trim());
+    }
+
+}
+
+
+
+
+
+
